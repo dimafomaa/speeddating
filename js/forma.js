1a199751b713d214f847d59d784a8943ffb0517f
Dima <103756308+dimafomaa@users.noreply.github.com>
2023-08-17 15:49:03 +0300

    Add files via upload

--- js/forma.js
+++ js/forma.js
@@ -1,54 +1,61 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $('input[type="tel"]').inputmask("+38 (999) 999-99-99");
 
-	$('input[type="tel"]').inputmask("+38 (999) 999-99-99");
+    $('form').on('submit', function (e) {
+        e.preventDefault(); // предотвращение стандартного поведения формы
+        let form = $(this);
 
-	$('form').on('submit', function (e) {
-		e.preventDefault(); // предотвращение стандартного поведения формы
-		let form = $(this);
+        // Сброс классов ошибок перед проверкой и отправкой формы
+        form.find('.error').removeClass('error');
 
-		
-		let phoneNumberInput = form.find('input[type="tel"]');// Проверка, что номер телефона полностью введен
-		let phoneNumber = phoneNumberInput.val();
-		let phoneNumberWithoutFormatting = phoneNumber.replace(/\D/g, ''); // Удаление всех нецифровых символов
+        // Проверяем все поля на заполненность и добавляем класс ошибки при необходимости
+        let hasEmptyField = false;
+        form.find('input').each(function() {
+            let input = $(this);
+            if (input.hasClass('required') && input.val().trim() === '') {
+                input.addClass('error');
+                hasEmptyField = true;
+            }
+        });
 
-		if (phoneNumberWithoutFormatting.length !== 12) {
-			
-			phoneNumberInput.addClass('error');// Если номер телефона не полный (12 цифр), бордер красным
-			return;
-		}
+        // Проверяем телефон
+        let phoneNumberInput = form.find('input[type="tel"]');
+        let phoneNumber = phoneNumberInput.val();
+        let phoneNumberWithoutFormatting = phoneNumber.replace(/\D/g, ''); // Удаление всех нецифровых символов
 
-		// Если номер телефона полный, удалить класс ошибки
-		phoneNumberInput.removeClass('error');
+        if (phoneNumberWithoutFormatting.length !== 12) {
+            phoneNumberInput.addClass('error');
+            hasEmptyField = true;
+        }
 
+        if (hasEmptyField) {
+            return;
+        }
 
-        // спам-блокировка кнопки отправки формы
-		form.find('button[type="submit"]').prop('disabled', true);
-		
-		
-		let loadingIcon = form.find('.loading-icon');
-        loadingIcon.addClass('active'); // показ иконки loading
-		
-		// отправка формы с помощью AJAX
-		$.ajax({
-			url: 'send-mail.php',
-			type: 'POST',
-			data: form.serialize(),
-			success: function (data) {
-				form[0].reset(); // очистка формы
-				// Открываем модальное окно благодарности после успешной отправки
-				const thankPopup = document.getElementById('modal-thank');
-				popupOpen(thankPopup);
-				
-				loadingIcon.removeClass('active');
-				
-			},
-			
-			complete: function() {
-				// разблокировка кнопки отправки формы
-				form.find('button[type="submit"]').prop('disabled', false);
-			}
-			
-		});
-	});
-	
+        form.find('button[type="submit"]').prop('disabled', true);
+
+        let loadingIcon = form.find('.loading-icon');
+        loadingIcon.addClass('active');
+
+        $.ajax({
+            url: 'send-mail.php',
+            type: 'POST',
+            data: form.serialize(),
+            success: function (data) {
+                form[0].reset();
+                const thankPopup = document.getElementById('modal-thank');
+                popupOpen(thankPopup);
+                loadingIcon.removeClass('active');
+            },
+
+            complete: function () {
+                form.find('button[type="submit"]').prop('disabled', false);
+            }
+        });
+    });
+
+    // Добавляем проверку событий для полей ввода, чтобы убрать класс ошибки, когда пользователь начинает вводить данные
+    $('form input').on('input', function() {
+        $(this).removeClass('error');
+    });
 });
